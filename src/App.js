b9fb95f1f05fc82bd78deb89af44019a65e79f0d
JEETBHARDWAJ <bhardwajjeet408@gmail.com>
2023-10-03 22:49:38 +0530

    TextForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+
 
 
 
 function App() {
   return (
-  <>
-  <Navbar title="SIH" />
-  </>
+    <>
+      <Navbar title="SIH" />
+      <TextForm/>
+    </>
 
 
   );
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -4,9 +4,9 @@ export default function TextForm() {
     return (
         <div>
 
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <div className="mb-3">
+                <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
         </div>
     )
