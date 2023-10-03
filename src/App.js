35cb1cfff1f21780e40f87a14903afb81f79932f
JEETBHARDWAJ <bhardwajjeet408@gmail.com>
2023-10-03 22:45:53 +0530

    addng TextForm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar';
-import TextForm from './component/Textarea';
 
 
 
 function App() {
   return (
   <>
-
-  <Navbar title="text" />
-
-  {/* <TextForm/> */}
-
+  <Navbar title="SIH" />
   </>
 
 
