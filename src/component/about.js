2e0dd556b645c6989e3400dd6b9574f28f7e7ce3
JEETBHARDWAJ <bhardwajjeet408@gmail.com>
2023-10-06 23:36:06 +0530

    all codes going wong

--- src/component/about.js
+++ src/component/about.js
@@ -3,37 +3,9 @@ import React from 'react';
 function About() {
 
     return (
-        <div className="container">
-        <header>
-            <h1>My Responsive Page</h1>
-        </header>
-        <nav>
-            <ul className="nav">
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Services</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
-        <section>
-            <h2>Welcome to our website</h2>
-            <p>This is a responsive page created using Bootstrap.</p>
-        </section>
-        <footer>
-            <p>&copy; 2023 Your Company. All rights reserved.</p>
-        </footer>
-    </div>
 
     );
-    
+
 }
 
 export default About;
